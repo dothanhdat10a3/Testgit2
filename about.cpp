1132ff62104fca90ba6a8a908910c58650062a59
dothanhdat10a3 <86611926+dothanhdat10a3@users.noreply.github.com>
2021-07-11 18:30:14 +0700

    Update about.cpp

--- about.cpp
+++ about.cpp
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
-//test thử thêm file
-#include <iostream>
-=======
+
 #include <conio.h>
 #include <studio.h>
 
 using namespace std;
 // ahihi co y de conflict
->>>>>>> 4e507e803f211b2c4d22b8a06e1cef290efa2dbf
+
